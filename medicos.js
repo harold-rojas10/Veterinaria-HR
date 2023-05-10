c6c955abd45edee79e6e389223499b1fbe25a149
harold-rojas10 <130325995+harold-rojas10@users.noreply.github.com>
2023-05-10 16:24:47 +0000

    Cambios_Replit

--- medicos.js
+++ medicos.js
@@ -2,13 +2,13 @@ let tablaMedico = document.getElementById("tabla-medicos");
 let cuerpoTabla = tablaMedico.querySelector("tbody");
 
 //Arreglos
-let nombreMed = ["Luna", "Orion", "Maily", "Mystic"]
-let apellidoMed = ["Ana", "Jorge", "Kevin", "8 años"]
+let nombreMed = ["Camilo", "Sofia", "Mario", "Andres"]
+let apellidoMed = ["Rojas", "Pineda", "Diaz", "llanos"]
 let cedula = ["1234567", "6789015", "5678954", "56723982"]
-let especialidad = ["5 años", "2 meses", "2 años", "8 años"]
-let consultorio = ["3105678946", "12345678909", "5678902567", "5678952567"]
-let correo = ["Veterinaria Conductual", "Medicina interna", "Dermatología veterinaria", "Farmacología veterinaria"]
-let pacientes = ["Jorge", "Daniela", "Andres", "Jeison"]
+let especialidad = ["Veterinaria Conductual", "Medicina interna", "Dermatología veterinaria", "Farmacología veterinaria"]
+let consultorio = ["101", "102", "201", "202"]
+let correo = ["CR@Veterinaria", "CR@Veterinaria", "CR@Veterinaria", "CR@Veterinaria"]
+let pacientes = ["Luna", "Orion", "Maily", "Mystic"]
 
 
 for (let i = 0; i < nombreMed.length; i++) {
